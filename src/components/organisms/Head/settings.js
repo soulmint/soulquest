ffbfd7b1d5cc34ffc93eb309fef4a8d5204f5693
Kien Duong <kienduong.hust@gmail.com>
2022-10-12 13:37:48 +0700

    update config social

--- src/components/organisms/Head/settings.js
+++ src/components/organisms/Head/settings.js
@@ -1,11 +1,11 @@
 const settings = {
   meta: {
-    rootUrl: 'http://localhost:5000',
+    rootUrl: `${process.env.PUBLIC_URL}`,
     title: 'Soulmint App',
     description: 'The reward-focused platform for verifiable credentials.',
     social: {
       graphic:
-        'https://cheatcode-assets.s3.amazonaws.com/default-social-graphic.png',
+        'https://pbs.twimg.com/profile_images/1578282623276748801/geGfPXtp_400x400.jpg',
       twitter: '@SoulmintApp'
     }
   }
