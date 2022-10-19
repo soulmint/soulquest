c31c0fa0933cbc8e71b33c768391e939bc54f5d0
Trung Chu <trungch@joomsolutions.com>
2022-10-19 10:27:38 +0700

    Improve footer responsive layout
    Improve footer responsive layout

--- src/components/organisms/Footer/footer.js
+++ src/components/organisms/Footer/footer.js
@@ -58,7 +58,7 @@ const Footer = (_props) => {
       <footer
         className={`${classes[rootClassName]} py-4 px-4 lg:px-0 space-x-4 z-10 text-sm font-medium`}
       >
-        <div className="container max-w-screen-xl mx-auto flex flex-wrap flex-col md:flex-row justify-between items-center px-4">
+        <div className="container max-w-screen-xl mx-auto flex flex-wrap flex-row justify-between items-center px-4">
           <div className="block text-sm text-gray-500 w-full md:w-auto text-center md:text-left dark:text-gray-400">
             <span>
               &copy; {year}{' '}
