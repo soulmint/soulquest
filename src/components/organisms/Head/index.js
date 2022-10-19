98c7a7a9eafb695091e69ecf14b3191c2a79229e
Qvv <quynhvv@joomsolutions.com>
2022-10-19 13:06:24 +0700

    Fine-tuning for meta data > icons

--- src/components/organisms/Head/index.js
+++ src/components/organisms/Head/index.js
@@ -112,8 +112,36 @@ const HeadCustom = (props) => {
         name="robots"
         content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"
       />
-      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/touch-icon-iphone-retina.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/icons/touch-icon-ipad.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="/icons/touch-icon-ipad-retina.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      />
       <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   );
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,8 +26,14 @@ class SoulMintDocument extends Document {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
