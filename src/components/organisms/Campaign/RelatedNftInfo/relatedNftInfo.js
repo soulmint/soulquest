3b28c3543eb55bfa69086939d64c937eeb312069
Qvv <quynhvv@joomsolutions.com>
2022-10-20 11:40:10 +0700

    Fine-tuning

--- src/components/organisms/Campaign/RelatedNftInfo/relatedNftInfo.js
+++ src/components/organisms/Campaign/RelatedNftInfo/relatedNftInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { array, bool, shape, string } from 'prop-types';
-import { useTranslation } from 'next-i18next';
+// import { useTranslation } from 'next-i18next';
 import defaultClasses from './relatedNftInfo.module.css';
 import { useStyle } from 'src/components/classify';
 import useThemes from 'src/hooks/useThemes';
@@ -18,52 +18,57 @@ const RelatedNftInfo = (props) => {
 
   const classes = useStyle(defaultClasses, propClasses);
 
-  const { t } = useTranslation('campaign_details');
+  // const { t } = useTranslation('campaign_details');
 
   const { rootClassName } = useThemes();
 
-  let child = null;
-
-  child = nftCollections.length
-    ? nftCollections.map((nftCollection, index) => (
-        <div key={index} className={`${classes.nftCollectionWrap}`}>
-          {showChainName ? (
-            <span
-              title={getChainName(nftCollection.nft_collection_id.chain_name)}
-            >
-              <span className={classes.chainName}>
-                {getChainName(nftCollection.nft_collection_id.chain_name)}
-              </span>
-            </span>
-          ) : null}
-
-          {showCollectionLink ? (
-            <TextLink
-              className={classes.nftCollectionLink}
-              href={`/nft-collection-details/${nftCollection.nft_collection_id.slug}`}
-            >
-              <span
-                className={`${classes.collectionName} ${
-                  classes[nftCollection.nft_collection_id.chain_name]
-                }`}
-              >
-                {nftCollection.nft_collection_id.name}
-              </span>
-              {showSmcAdd ? (
-                <span className={classes.contractAdd}>
-                  (
-                  {ellipsify({
-                    str: nftCollection.nft_collection_id.contract_address,
-                    start: 4,
-                    end: 4
-                  })}
-                  )
+  let total = 0;
+  const child = nftCollections.length
+    ? nftCollections.map((nftCollection, index) => {
+        total++;
+        const chainName = getChainName(
+          nftCollection.nft_collection_id.chain_name
+        );
+        const separator = total < nftCollections.length ? ',' : null;
+        const rs = (
+          <div key={index} className={`${classes.nftCollectionWrap}`}>
+            {showChainName ? (
+              <span title={chainName}>
+                <span className={classes.chainName}>
+                  {chainName} {separator}
                 </span>
-              ) : null}
-            </TextLink>
-          ) : null}
-        </div>
-      ))
+              </span>
+            ) : null}
+            {showCollectionLink ? (
+              <TextLink
+                className={classes.nftCollectionLink}
+                href={`/nft-collection-details/${nftCollection.nft_collection_id.slug}`}
+              >
+                <span
+                  className={`${classes.collectionName} ${
+                    classes[nftCollection.nft_collection_id.chain_name]
+                  }`}
+                >
+                  {nftCollection.nft_collection_id.name}
+                </span>
+                {showSmcAdd ? (
+                  <span className={classes.contractAdd}>
+                    (
+                    {ellipsify({
+                      str: nftCollection.nft_collection_id.contract_address,
+                      start: 4,
+                      end: 4
+                    })}
+                    )
+                  </span>
+                ) : null}
+              </TextLink>
+            ) : null}
+          </div>
+        );
+
+        return rs;
+      })
     : null;
 
   return <div className={`${classes[rootClassName]}`}>{child}</div>;
